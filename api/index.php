472da8544519a0919a21038055a7448cd3ef4960
Diki Setiawan <dikisetia101@gmail.com>
2025-02-07 14:30:42 +0700

    update

--- api/index.php
+++ api/index.php
@@ -10,7 +10,7 @@ include "../function/koneksi.php";
 $api_url = "https://vtsapi.easygo-gps.co.id/api/report/lastpositionold";
 $token = "C3639EADE6D24E1481D7BFBDAB918429";
 
-$input_nopol= '["B 9625 feu"];';
+$input_nopol= '["B 9505 feu"];';
 
 
 // ambil data dari url
@@ -64,74 +64,24 @@ curl_setopt($curl, CURLOPT_HTTPHEADER, array(
       $sql = "SELECT * FROM report_unit";
       $data_query = $koneksi->query($sql);
 
-    
 
       if(!empty($data_query) && $data_query -> num_rows > 0){
 
         $sql = "DELETE FROM report_unit";
         $koneksi->query($sql);
-        if (mysqli_query($koneksi, $sql)) {
-          echo "Data berhasil di hapus";
-        }else{
-            echo "Data gagal di hapus";
-        }
 
-
-      }else{
-
-         // input data ke db myapi
+        // input data ke db myapi
        $sql = "INSERT INTO report_unit (no_pol, no_aset, msisdn, lat, lon, address, gps_time, received_time, geofence_location, speed, direction, acc, odometer, status_vehicle, report_status, alarm_status,temperature1,temperature2,mainpowervoltage,backupbatteryvoltage,sos , gsm_signal,  rfid_driver, driver, start_moving, start_parking, start_idle, company_nm, can_elements, do_header  ) VALUES ('$no_pol', '$no_aset', '$msisdn', '$lat', '$lon', '$address', '$gps_time', '$received_time', '$geofence_location', '$speed', '$direction', '$acc', '$odometer', '$status_vehicle', '$report_status', '$alarm_status','$temperature1','$temperature2','$mainpowervoltage','$backupbatteryvoltage','$sos' , '$gsm_signal',  '$rfid_driver', '$driver', '$start_moving', '$start_parking', '$start_idle', '$company_nm', '$can_elements', '$do_header' )";
     
        //logika input data
        if (mysqli_query($koneksi, $sql)) {
-           echo "Data berhasil di tambahkan";
+           echo "Data berhasil di update";
        }else{
-           echo "Data gagal di tambahkan";
+           echo "Data gagal di update";
        }
-      // update data ke db myapi, jika data sudah ada
-        // $sql = "UPDATE report_unit SET no_pol = '$row[no_pol]',
-        //                               no_aset = '$row[no_aset]',
-        //                               msisdn = '$row[msisdn]',
-        //                               lat = '$row[lat]',
-        //                               lon = '$row[lon]',
-        //                               address = '$row[address]',
-        //                               gps_time = '$row[gps_time]',
-        //                               received_time = '$row[received_time]',
-        //                               geofence_location = '$row[geofence_location]',
-        //                               speed = '$row[speed]',
-        //                               direction = '$row[direction]',
-        //                               acc = '$row[acc]',
-        //                               odometer = '$row[odometer]',
-        //                               status_vehicle = '$row[status_vehicle]',
-        //                               report_status = '$row[report_status]',
-        //                               alarm_status = '$row[alarm_status]',
-        //                               temperature1 = '$row[temperature1]',
-        //                               temperature2 = '$row[temperature2]',
-        //                               mainpowervoltage = '$row[mainpowervoltage]',
-        //                               backupbatteryvoltage = '$row[backupbatteryvoltage]',
-        //                               sos = '$row[sos]',
-        //                               gsm_signal = '$row[gsm_signal]',
-        //                               rfid_driver = '$row[rfid_driver]',
-        //                               driver = '$row[driver]',
-        //                               start_moving = '$row[start_moving]',
-        //                               start_parking = '$row[start_parking]',
-        //                               start_idle = '$row[start_idle]',
-        //                               company_nm = '$row[company_nm]',
-        //                               can_elements = '$row[can_elements]',
-        //                               do_header = '$row[do_header]'
 
-        //                               WHERE no_pol='$no_pol'";
-
-        //                                 //logika update data
-        //                               if (mysqli_query($koneksi, $sql)) {
-        //                                   echo "Data berhasil di update";
-        //                               }else{
-        //                                   echo "Data gagal di update";
-        //                               }
-
+      }
   
-
-  }
 }
 }
 
